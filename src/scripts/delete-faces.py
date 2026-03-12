aa0bf8ad8479595021c2b5a7ecd661a6daf7de98
Nate Wright <natew@notthisway.com>
2026-03-12 11:13:16 +0000

    Use .glb file with just the outer faces of the countries

---
+++ src/scripts/delete-faces.py
@@ -0,0 +1,23 @@
+# Blender script to delete all faces of a mesh
+# except the "surface" meshes of the countries
+#
+# This changes the meshes from extruded shapes
+# to "flat" planes to reduce polycount.
+#
+# @see https://blender.stackexchange.com/questions/239466/face-selection-by-condition
+
+import bpy
+import bmesh
+from math import radians
+
+context = bpy.context
+
+for ob in bpy.context.scene.objects:
+
+    me = ob.data
+
+    bm = bmesh.from_edit_mesh(me)
+    for f in bm.faces:
+        f.select_set(f.normal.angle(f.calc_center_median()) > radians(80))
+
+    bmesh.update_edit_mesh(me)
